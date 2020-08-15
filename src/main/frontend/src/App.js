32fd4f18378536a4eadcf9d11ce94ca6f6bb93c1
hummai <shining.melon2015@gmail.com>
2020-08-15 16:41:00 +0300

    Added init frontend

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div className="App">
+            <header className="header">
+                <div className="header-menu-btn">Main page</div>
+                <div className="header-menu-btn">News</div>
+                <div className="header-menu-btn">Messages</div>
+                <div className="header-menu-btn">Friends</div>
+            </header>
+
+            <div className="content">
+                <div className="other">
+
+                </div>
+
+                <div className="post-box">
+                    <div>
+                        <form action="" className="post-form">
+                            <textarea cols="100" rows="5" className="post-textarea"></textarea>
+                            <input type="submit" value="Create post"/>
+                        </form>
+                    </div>
+
+                    <div className="post-list">
+
+                    </div>
+                </div>
+            </div>
+
+            <footer className="footer">
+                footer
+            </footer>
+        </div>
+    );
+};
 
 export default App;
